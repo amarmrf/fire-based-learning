d5b38d45c8e8128357d20a34c53b32ec5d9da2e7
Muhammad Amar <amarmarf@gmail.com>
2020-10-21 23:41:24 +0700

    Add files via upload

---
+++ util/config.js
@@ -0,0 +1,10 @@
+module.exports =  {
+    apiKey: "AIzaSyBcroMHXGpMnQ587DdHwpgKmHi5JKxkNZo",
+    authDomain: "soil-b7fde.firebaseapp.com",
+    databaseURL: "https://soil-b7fde.firebaseio.com",
+    projectId: "soil-b7fde",
+    storageBucket: "soil-b7fde.appspot.com",
+    messagingSenderId: "916682291130",
+    appId: "1:916682291130:web:f4c55157fe1a684bf00f83",
+    measurementId: "G-RBN15Z6B7Z"
+  }
